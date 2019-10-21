c8e629a33d731257be49cc1edff0fb96f9732b7f
Luis Diego Mora Aguilar <39139078+LordGrapha@users.noreply.github.com>
2019-10-21 17:51:05 -0600

    last commit
    Falto el dibujo de la linea y la animación con el recorrido :(

--- src/gui/VentanaPrincipal.java
+++ src/gui/VentanaPrincipal.java
@@ -7,7 +7,16 @@ package gui;
 
 import caso.extra.MapManager;
 import caso.extra.Nodo;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
 import java.util.Hashtable;
 import java.util.Observable;
 import java.util.Observer;
@@ -25,15 +34,202 @@ public class VentanaPrincipal extends javax.swing.JFrame implements Observer{
     
     private MapManager nuevoMap;
     private Hashtable<JButton, Nodo<Point>> hashMap;
+    private Graphics g;
+    private final String letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    private int letra;
+    private Nodo<Point> primero;
 
     /**
      * Creates new form VentanaPrincipal
      */
     public VentanaPrincipal() {
         initComponents();
+        this.setTitle("Caso Extra - Estructuras de Datos");
         nuevoMap = new MapManager();
         nuevoMap.addObserver(this);
         hashMap = new Hashtable();
+        letra = 0;
+        g = new Graphics() {
+            @Override
+            public Graphics create() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void translate(int i, int i1) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public Color getColor() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void setColor(Color color) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void setPaintMode() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void setXORMode(Color color) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public Font getFont() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void setFont(Font font) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public FontMetrics getFontMetrics(Font font) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public Rectangle getClipBounds() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void clipRect(int i, int i1, int i2, int i3) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void setClip(int i, int i1, int i2, int i3) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public Shape getClip() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void setClip(Shape shape) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void copyArea(int i, int i1, int i2, int i3, int i4, int i5) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void drawLine(int i, int i1, int i2, int i3) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void fillRect(int i, int i1, int i2, int i3) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void clearRect(int i, int i1, int i2, int i3) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void drawRoundRect(int i, int i1, int i2, int i3, int i4, int i5) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void fillRoundRect(int i, int i1, int i2, int i3, int i4, int i5) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void drawOval(int i, int i1, int i2, int i3) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void fillOval(int i, int i1, int i2, int i3) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void drawArc(int i, int i1, int i2, int i3, int i4, int i5) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void fillArc(int i, int i1, int i2, int i3, int i4, int i5) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void drawPolyline(int[] ints, int[] ints1, int i) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void drawPolygon(int[] ints, int[] ints1, int i) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void fillPolygon(int[] ints, int[] ints1, int i) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void drawString(String string, int i, int i1) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void drawString(AttributedCharacterIterator aci, int i, int i1) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public boolean drawImage(Image image, int i, int i1, ImageObserver io) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public boolean drawImage(Image image, int i, int i1, int i2, int i3, ImageObserver io) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public boolean drawImage(Image image, int i, int i1, Color color, ImageObserver io) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public boolean drawImage(Image image, int i, int i1, int i2, int i3, Color color, ImageObserver io) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public boolean drawImage(Image image, int i, int i1, int i2, int i3, int i4, int i5, int i6, int i7, ImageObserver io) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public boolean drawImage(Image image, int i, int i1, int i2, int i3, int i4, int i5, int i6, int i7, Color color, ImageObserver io) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void dispose() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
     }
     
     @Override
@@ -42,23 +238,23 @@ public class VentanaPrincipal extends javax.swing.JFrame implements Observer{
         System.out.println("" + arg);
         Nodo<Point> nodo = (Nodo<Point>) arg;
         //ImageIcon icon = new ImageIcon("Resources/images.png", "Icono");
-        JButton label = new JButton("");
-        
-        label.setBounds(nodo.getValor().x-15, nodo.getValor().y-15, 15, 15);
-        label.addMouseListener(new java.awt.event.MouseAdapter(){
+        JButton nuevoPunto = new JButton(letras.substring(letra, letra + 1));
+        nuevoPunto.setFont(new Font("Verdana", Font.PLAIN, 8));
+        letra++;
+        nuevoPunto.setBounds(nodo.getValor().x-20, nodo.getValor().y-45, 35, 35);
+        nuevoPunto.addMouseListener(new java.awt.event.MouseAdapter(){
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 actionPerformed(evt);
             }
         });
         
-        hashMap.put(label, nodo);
-        this.add(label);
+        hashMap.put(nuevoPunto, nodo);
+        mapaCiudad.add(nuevoPunto);
         repaint();
-        
     }
     
     private void actionPerformed(java.awt.event.MouseEvent evt){
-        Nodo<Point> nodoSelected = hashMap.get((JLabel) evt.getSource());
+        Nodo<Point> nodoSelected = hashMap.get((JButton) evt.getSource());
         nuevoMap.agregarArco(nodoSelected);
     }
     
@@ -74,74 +270,70 @@ public class VentanaPrincipal extends javax.swing.JFrame implements Observer{
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        mapaCiudad = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         jLabel1.setText("jLabel1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(700, 700));
+        setPreferredSize(new java.awt.Dimension(800, 550));
         getContentPane().setLayout(null);
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Resources/ciudad2.0.png"))); // NOI18N
-        jLabel2.setText("jLabel2");
-        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+        mapaCiudad.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Resources/ciudad2.0.png"))); // NOI18N
+        mapaCiudad.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jLabel2MouseClicked(evt);
+                mapaCiudadMouseClicked(evt);
             }
         });
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(10, 25, 486, 334);
+        getContentPane().add(mapaCiudad);
+        mapaCiudad.setBounds(-4, -1, 800, 470);
+
+        jButton1.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
+        jButton1.setText("Inicio");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(360, 480, 73, 23);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
+    private void mapaCiudadMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_mapaCiudadMouseClicked
         // TODO add your handling code here:
         
         int x = evt.getLocationOnScreen().x;
         int y = evt.getLocationOnScreen().y;
 
-        System.out.println("Estoy en X: " + x + "\nY: " + y);
-        Nodo<Point> obj = nuevoMap.agregarPunto(x, y);
-    }//GEN-LAST:event_jLabel2MouseClicked
+        Nodo<Point> ini = nuevoMap.getUltiAgregado();
+        
+        Nodo<Point> nuevo = nuevoMap.agregarPunto(x, y);
+        if(ini == null){
+            primero = nuevo;
+        }
+        
+        /*if(ini != nuevo){
+            g.drawLine(ini.getValor().x, ini.getValor().y,
+                    nuevo.getValor().x, nuevo.getValor().y);
+        }*/
+    }//GEN-LAST:event_mapaCiudadMouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        nuevoMap.anchuraDelGrafo(primero);
+        nuevoMap.profundidadDelGrafo(primero);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new VentanaPrincipal().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel mapaCiudad;
     // End of variables declaration//GEN-END:variables
 }
